b2fe34b8474aa6c15ddba8499a738c25f61afc78
paulLiaras <paul.liaras@student.junia.com>
2026-06-18 17:20:21 +0200

    test USB hexa ok sans bruit sans effet

--- src/main_USB.cpp
+++ src/main_USB.cpp
@@ -4,26 +4,37 @@
 #ifndef USB_AUDIO
 #error "Erreur : La pile USB_AUDIO n'est pas activée dans platformio.ini !"
 #endif
+#if defined(AUDIO_USB_CHANNEL_COUNT) && AUDIO_USB_CHANNEL_COUNT != 6
+#error "Erreur : Le nombre de canaux USB n'est pas configuré sur 6 !"
+#endif
 
 // Objets communs
-AudioInputUSB               usbIn;          // Entrée USB Stéréo (depuis le PC)
-AudioOutputUSB              usbOut;         // Sortie USB Stéréo (vers le PC)
+AudioInputUSBHex            usbIn;          // Entrée USB Hexaphonique (depuis le PC)
+AudioOutputUSBHex           usbOut;         // Sortie USB Hexaphonique (vers le PC)
 
 // --- LES INTERFACES TDM2 ---
 AudioOutputTDM2             tdmOut;
 AudioInputTDM2              tdmIn;
 
 // ===================================================================
-// LE ROUTAGE STÉRÉO (PC -> USB -> TDM2 -> DAISY -> TDM2 -> USB -> PC)
+// LE ROUTAGE HEXAPHONIQUE (PC -> USB -> TDM2 -> DAISY -> TDM2 -> USB -> PC)
 // ===================================================================
 
-// A) USB IN (Stéréo) -> TDM OUT (Vers Daisy, ports pairs 0 et 2 = slots 0 et 1)
-AudioConnection patchUsbToTdmL(usbIn, 0, tdmOut, 0);
-AudioConnection patchUsbToTdmR(usbIn, 1, tdmOut, 2);
+// A) USB IN -> TDM OUT (Vers Daisy, ports pairs 0, 2, 4, 6, 8, 10 = slots 0 à 5)
+AudioConnection patchUsbToTdm1(usbIn, 0, tdmOut, 0);
+AudioConnection patchUsbToTdm2(usbIn, 1, tdmOut, 2);
+AudioConnection patchUsbToTdm3(usbIn, 2, tdmOut, 4);
+AudioConnection patchUsbToTdm4(usbIn, 3, tdmOut, 6);
+AudioConnection patchUsbToTdm5(usbIn, 4, tdmOut, 8);
+AudioConnection patchUsbToTdm6(usbIn, 5, tdmOut, 10);
 
-// B) TDM IN (Depuis Daisy, ports pairs 0 et 2) -> USB OUT (Stéréo vers PC)
-AudioConnection patchTdmToUsbL(tdmIn, 0, usbOut, 0);
-AudioConnection patchTdmToUsbR(tdmIn, 2, usbOut, 1);
+// B) TDM IN -> USB OUT (Depuis Daisy, ports pairs 0, 2, 4, 6, 8, 10 vers PC)
+AudioConnection patchTdmToUsb1(tdmIn, 0, usbOut, 0);
+AudioConnection patchTdmToUsb2(tdmIn, 2, usbOut, 1);
+AudioConnection patchTdmToUsb3(tdmIn, 4, usbOut, 2);
+AudioConnection patchTdmToUsb4(tdmIn, 6, usbOut, 3);
+AudioConnection patchTdmToUsb5(tdmIn, 8, usbOut, 4);
+AudioConnection patchTdmToUsb6(tdmIn, 10, usbOut, 5);
 
 // ===================================================================
 // SETUP & LOOP
