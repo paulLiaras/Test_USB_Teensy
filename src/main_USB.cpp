630f99327c0fe66ff9010c086c89fe3a561383d8
paulLiaras <paul.liaras@student.junia.com>
2026-06-18 16:53:25 +0200

    test tdm stéréo USB ok sans effet

--- src/main_USB.cpp
+++ src/main_USB.cpp
@@ -1,63 +1,36 @@
 #include <Audio.h>
 
-
- #define USE_STEREO_MIX 
-
-
 // Sécurités
 #ifndef USB_AUDIO
 #error "Erreur : La pile USB_AUDIO n'est pas activée dans platformio.ini !"
 #endif
-#if defined(AUDIO_USB_CHANNEL_COUNT) && AUDIO_USB_CHANNEL_COUNT != 6
-#error "Erreur : Le nombre de canaux USB n'est pas configuré sur 6 !"
-#endif
 
 // Objets communs
-AudioInputUSBHex            usbIn;          
-AudioOutputUSBHex           usbOut;         
+AudioInputUSB               usbIn;          // Entrée USB Stéréo (depuis le PC)
+AudioOutputUSB              usbOut;         // Sortie USB Stéréo (vers le PC)
+
+// --- LES INTERFACES TDM2 ---
+AudioOutputTDM2             tdmOut;
+AudioInputTDM2              tdmIn;
 
 // ===================================================================
-// ARCHITECTURE CONDITIONNELLE
+// LE ROUTAGE STÉRÉO (PC -> USB -> TDM2 -> DAISY -> TDM2 -> USB -> PC)
 // ===================================================================
-#ifdef USE_STEREO_MIX
-  // --- MODE STÉRÉO ---
-  AudioMixer4              mixerLeft;
-  AudioMixer4              mixerRight;
 
-  AudioConnection          patchCord1(usbIn, 0, mixerLeft, 0); 
-  AudioConnection          patchCord2(usbIn, 1, mixerLeft, 1); 
-  AudioConnection          patchCord3(usbIn, 2, mixerLeft, 2); 
-  
-  AudioConnection          patchCord4(usbIn, 3, mixerRight, 0); 
-  AudioConnection          patchCord5(usbIn, 4, mixerRight, 1); 
-  AudioConnection          patchCord6(usbIn, 5, mixerRight, 2); 
+// A) USB IN (Stéréo) -> TDM OUT (Vers Daisy, ports pairs 0 et 2 = slots 0 et 1)
+AudioConnection patchUsbToTdmL(usbIn, 0, tdmOut, 0);
+AudioConnection patchUsbToTdmR(usbIn, 1, tdmOut, 2);
 
-  AudioConnection          patchCord7(mixerLeft, 0, usbOut, 0);
-  AudioConnection          patchCord8(mixerRight, 0, usbOut, 1);
-
-#else
-  // --- MODE HEXAPHONIQUE (Miroir direct) ---
-  AudioConnection          patchCord1(usbIn, 0, usbOut, 0); 
-  AudioConnection          patchCord2(usbIn, 1, usbOut, 1); 
-  AudioConnection          patchCord3(usbIn, 2, usbOut, 2); 
-  AudioConnection          patchCord4(usbIn, 3, usbOut, 3); 
-  AudioConnection          patchCord5(usbIn, 4, usbOut, 4); 
-  AudioConnection          patchCord6(usbIn, 5, usbOut, 5); 
-#endif
+// B) TDM IN (Depuis Daisy, ports pairs 0 et 2) -> USB OUT (Stéréo vers PC)
+AudioConnection patchTdmToUsbL(tdmIn, 0, usbOut, 0);
+AudioConnection patchTdmToUsbR(tdmIn, 2, usbOut, 1);
 
 // ===================================================================
 // SETUP & LOOP
 // ===================================================================
 void setup() {
+  Serial.begin(9600);
   AudioMemory(60); 
-
-#ifdef USE_STEREO_MIX
-  // Application du gain uniquement si le mixeur existe
-  for (int i = 0; i < 3; i++) {
-    mixerLeft.gain(i, 0.4); 
-    mixerRight.gain(i, 0.4);
-  }
-#endif
 }
 
 void loop() {
